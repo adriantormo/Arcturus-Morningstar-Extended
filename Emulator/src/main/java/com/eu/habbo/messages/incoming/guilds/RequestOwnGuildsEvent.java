e94068c3e061a7f35998aa2b488d69981fbf213a
Adrián Tormo <adrian@tormo.me>
2026-06-01 21:49:06 +0200

    fix: preserve own guild memberships

--- Emulator/src/main/java/com/eu/habbo/messages/incoming/guilds/RequestOwnGuildsEvent.java
+++ Emulator/src/main/java/com/eu/habbo/messages/incoming/guilds/RequestOwnGuildsEvent.java
@@ -7,6 +7,10 @@ import com.eu.habbo.messages.incoming.MessageHandler;
 import com.eu.habbo.messages.outgoing.guilds.GuildListComposer;
 import gnu.trove.set.hash.THashSet;
 
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class RequestOwnGuildsEvent extends MessageHandler {
     @Override
     public int getRatelimit() {
@@ -16,13 +20,33 @@ public class RequestOwnGuildsEvent extends MessageHandler {
     @Override
     public void handle() throws Exception {
         THashSet<Guild> guilds = new THashSet<Guild>();
+        Set<Integer> guildIds = new HashSet<>();
         HabboStats stats = this.client.getHabbo().getHabboStats();
 
-        stats.guilds.clear();
+        for (int guildId : stats.guilds) {
+            if (guildId == 0)
+                continue;
 
-        for (Guild guild : Emulator.getGameEnvironment().getGuildManager().getGuilds(this.client.getHabbo().getHabboInfo().getId())) {
-            if (guild != null) {
+            Guild guild = Emulator.getGameEnvironment().getGuildManager().getGuild(guildId);
+
+            if (guild != null && guildIds.add(guild.getId())) {
                 guilds.add(guild);
+            }
+        }
+
+        List<Guild> persistedGuilds = Emulator.getGameEnvironment().getGuildManager().getGuilds(this.client.getHabbo().getHabboInfo().getId());
+        persistedGuilds.addAll(Emulator.getGameEnvironment().getGuildManager().getOwnedGuilds(this.client.getHabbo().getHabboInfo().getId()));
+
+        for (Guild guild : persistedGuilds) {
+            if (guild != null && guildIds.add(guild.getId())) {
+                guilds.add(guild);
+            }
+        }
+
+        if (!guilds.isEmpty()) {
+            stats.guilds.clear();
+
+            for (Guild guild : guilds) {
                 stats.addGuild(guild.getId());
             }
         }
